cca90b70994940e74838119fc4fecf5c9f8cfeae
Vaibhav Pawar <vp2897684@gmail.com>
2026-01-21 13:26:31 +0530

    Hello

--- Reverse_Array.java
+++ Reverse_Array.java
@@ -1,4 +1,15 @@
 public class Reverse_Array {
+
+    //With third Variable;
+    static void ReverseArray(int A[]){
+        for(int i = 0 ,j = A.length-1; i<j; i++,j--){
+            int temp = A[i];
+            A[i]=A[j];
+            A[j] = temp;
+        }
+    }
+
+    //Without third Variable;
     static void ArrayReverse(int A[]){
         for(int i = 0 , j = A.length-1; i<j; i++,j--){
             A[i] = A[i]+A[j];
@@ -13,7 +24,9 @@ public class Reverse_Array {
     }
     public static void main(String args[]){
         int A[] = {1,2,3,4,5};
-        ArrayReverse(A);
+        ReverseArray(A);
+        //Display(A);
+        //ArrayReverse(A);
         Display(A);
     }
 }
